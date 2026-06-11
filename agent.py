20059054a221d2bae66d37fa60d445fc16a221df
salmajebel <122825771+salmajebel@users.noreply.github.com>
2026-06-11 13:37:01 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,10 +1,16 @@
-from google import genai
 import os
+from google import genai
 
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
 
-chat = client.chats.create(model="gemini-3.5-flash")
-
 def ask_agent(message):
-    response = chat.send_message(message)
-    return response.text
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-3.5-flash",
+            contents=message
+        )
+        return response.text
+
+    except Exception as e:
+        return f"Error: {str(e)}"

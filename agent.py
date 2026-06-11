47f5b79523b0796e5f0f1460aef7186e2d4e4fe4
salmajebel <122825771+salmajebel@users.noreply.github.com>
2026-06-11 14:00:02 +0100

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,21 +1,15 @@
 import os
-
 from google import genai
 
+api_key = os.getenv("GOOGLE_API_KEY")
 
-
-client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
-
-
+client = genai.Client(
+    api_key=api_key
+)
 
 def ask_agent(message):
-
     response = client.models.generate_content(
-
         model="gemini-1.5-flash",
-
         contents=message
-
     )
-
-    return response.text 
+    return response.text
